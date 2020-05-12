690fed650547e8dc7e14ed84c6e82cee3869e508
Jamie Shiers <jamie@selling.io>
2020-05-12 12:38:20 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Activity;
 use App\Http\Requests\ActivityStoreRequest;
+use App\Models\Activity;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
