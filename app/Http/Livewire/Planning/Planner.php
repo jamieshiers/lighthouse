f4196278034a10f9150998eddbe6657f2117b04c
Jamie Shiers <jamie@jamieshiers.co.uk>
2020-05-13 19:24:57 +0100

    [WIP] - Basic Livewire Setup

---
+++ app/Http/Livewire/Planning/Planner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Planning;
+
+use Livewire\Component;
+
+class Planner extends Component
+{
+    public function render()
+    {
+        return view('livewire.planning.planner');
+    }
+}
